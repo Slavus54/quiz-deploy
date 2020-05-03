4bc06f73a3b9b960cd8cbb37efb5df108cc89226
Slavus54 <x.miros@yandex.ru>
2020-05-03 13:26:57 +0700

    Updates

---
+++ precache-manifest.fbcc5447437f16a1c690278f0789ea69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79fd28782b3d5682902721bae66760b",
+    "url": "/quiz-deploy/index.html"
+  },
+  {
+    "revision": "e40d40300639338369ea",
+    "url": "/quiz-deploy/static/css/main.d69f238d.chunk.css"
+  },
+  {
+    "revision": "e563b5b8218e444eb345",
+    "url": "/quiz-deploy/static/js/2.fb8053f4.chunk.js"
+  },
+  {
+    "revision": "e40d40300639338369ea",
+    "url": "/quiz-deploy/static/js/main.0a2b5fbc.chunk.js"
+  },
+  {
+    "revision": "5c28a28e76e8da96c1a8",
+    "url": "/quiz-deploy/static/js/runtime-main.76ce8cbe.js"
+  }
+]);
